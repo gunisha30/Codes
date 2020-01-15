8b0cead89f97de777acde7fe0fc48f0eb7038c62
Gunisha <gunisha3007@gmail.com>
2020-01-15 23:09:25 +0530

    added new file

---
+++ bleakno.py
@@ -0,0 +1,18 @@
+ def bin(p):
+    if (p>1):
+        bin(p//2)
+    a.append(p%2)
+    return(a.count(1))
+def f(n):
+    for k in range(1,n):
+        d=bin(k)
+        if (k + d==n):
+            return 1
+    return 0
+t=int(input())
+while(t):
+    n=int(input())
+    a=[]
+    i=f(n)
+    print(i)
+    t=t-1  
