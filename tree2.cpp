52d1b840943fe15f14be3cf15db34d3cb5d37460
Gunisha Chaturvedi <gunisha3007@gmail.com>
2020-02-19 22:23:33 +0530

    insertion operation on trees

---
+++ tree2.cpp
@@ -0,0 +1,48 @@
+#include<iostream.h>
+
+#include<conio.h>
+
+#define NULL 0
+
+class tree
+    
+{
+     
+public:
+            
+int value, *left, *right;  
+                       
+void insert(int);
+    
+};
+tree* tree::insert(int idata)
+            
+{
+            
+tree* obj;
+            
+obj->value=idata;
+            
+obj->left=NULL;
+            
+obj->right=NULL;
+            
+return obj;
+            
+}
+    
+
+void main()
+
+{
+    
+clrscr();
+tree* node=insert(9);
+    
+node->left=insert(10);
+    
+node->right=insert(11);
+    
+cout<<root->value;
+getch();
+}
