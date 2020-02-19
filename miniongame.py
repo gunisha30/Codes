96e238a37d63cff2b16f1abe75a0d191cf4a9a86
Gunisha Chaturvedi <gunisha3007@gmail.com>
2020-02-19 06:23:59 +0530

    Update miniongame.py

--- miniongame.py
+++ miniongame.py
@@ -3,25 +3,15 @@ def minion_game(s):
     l=[]
     l1=[]
     l2=[]
-    s1=""
     c1=0
     c2=0
-    for i in range(0,len(s)):
-        l.append(s[i])
-    for i in range (0,len(s)):
-        s1=l[i]
-        for j in range(i+1,len(s)):
-            s1=s1+l[j]
-            if s1 not in l:
-                l.append(s1)
-    #print(l)
+    l = [s[i: j] for i in range(len(s)) 
+          for j in range(i + 1, len(s) + 1)]
     for i in range(0,len(l)):
         if (l[i] not in l1) and (l[i][0] in ('A','E','I','O','U')):
             l1.append(l[i])
         if (l[i] not in l2) and (l[i][0] not in ('A','E','I','O','U')):
             l2.append(l[i])
-    #print(l1)
-    #print(l2)
     for i in l1:
         n=co(s,i)
         c1=c1+n
@@ -30,12 +20,19 @@ def minion_game(s):
         c2=c2+n
     if(c1>c2):
         print("Kevin {}".format(c1))
-    else: 
+    if(c1>c2):
+        print("Kevin {}".format(c1))
+    elif(c2>c1): 
         print("Stuart {}".format(c2))
+    else:
+        print("Draw")
 
-def co(strng,pat):
-    res = [i for i in range(len(strng)) if strng.startswith(pat, i)] 
-    return len(res)
+def co(string, sub_string):
+    c=0
+    for i in range(0,len(string)):
+        if(string[i:i+len(sub_string)]==sub_string):
+            c=c+1
+    return c
 if __name__ == '__main__':
-    s = raw_input()
+    s = input()
     minion_game(s)
