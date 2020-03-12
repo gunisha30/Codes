5d4d37d5f205f77adbb1835d7a31f86acf624406
Gunisha Chaturvedi <gunisha3007@gmail.com>
2020-03-12 11:45:25 +0530

    Update nmeetings.py

--- nmeetings.py
+++ nmeetings.py
@@ -6,19 +6,22 @@ while(t):
     size=int(input())
     a=list(map(int,input().split()))
     b=list(map(int,input().split()))
+    c=[]
+    for i in range(1,size+1):
+        c.append(i)
     def swap(l,i,j):
         t=l[i]
         l[i]=l[j]
         l[j]=t
-    for i in range(0,len(b)):
-        for j in range(i+1,len(b)):
+    for i in range(0,size):
+        for j in range(i+1,size):
             if(b[i]>b[j]):
                 swap(b,i,j)
                 swap(a,i,j)
                 swap(c,i,j)
     print(c[0],end=" ")
     i=0
-    for j in range(1,len(b)-1):
+    for j in range(1,size):
         if(a[j]>=b[i]):
             i=j
             print(c[j],end=" ")
