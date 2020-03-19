5f13a92071c064456010f91e00b1bea7a1355e57
Gunisha Chaturvedi <gunisha3007@gmail.com>
2020-03-19 18:50:19 +0530

    Update smallestint.py

--- smallestint.py
+++ smallestint.py
@@ -1,6 +1,5 @@
-# Question: It can be seen that the number, 125874, and its double, 251748, contain exactly the same digits, but in a
-# different order.
-# Find the smallest positive integer, x, such that 2x, 3x, 4x, 5x, and 6x, contain the same digits.
+Question: It can be seen that the number 125874, and its double, 251748, contain exactly the same digits, but in a
+different order. Find the smallest positive integer, x, such that 2x, 3x, 4x, 5x, and 6x, contain the same digits.
 i=1
 def f(l1,l2,l3,l4,l5):
     l1.sort()
