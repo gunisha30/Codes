99dc6ba3d31308db8eb30c9571d0d8d6c5a6b2d1
Gunisha Chaturvedi <gunisha3007@gmail.com>
2020-04-28 11:59:08 +0530

    Create palindrome.py

---
+++ palindrome.py
@@ -0,0 +1,13 @@
+s='madam'
+s1=s[::-1]
+mid=len(s)//2
+if(len(s)%2==0):
+    if(s[0:mid]==s1[0:mid]):
+        print('yes')
+    else:
+        print('no')
+else:
+    if(s[0:mid+1]==s1[0:mid+1]):
+        print('yes')
+    else:
+        print('no') 
