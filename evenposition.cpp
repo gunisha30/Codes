188a9cabcadf0c6f74c9b2ea9e08d37e114a4efc
Gunisha Chaturvedi <gunisha3007@gmail.com>
2020-06-14 23:50:42 +0530

    Add files via upload

---
+++ evenposition.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+#include<bits/stdc++.h>
+
+#include<cmath>
+
+using namespace std;
+
+
+int main()
+
+{
+    
+int a;
+    
+cin>>a;
+    
+float c;
+    
+c=log2(a);
+    
+cout<<(1<<int(c));
+    
+    
+return 0;
+
+}
