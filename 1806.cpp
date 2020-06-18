64ab81797ca19d53a222d0645a2d84ced0e54411
Gunisha Chaturvedi <gunisha3007@gmail.com>
2020-06-18 23:01:23 +0530

    Add files via upload

---
+++ 1806.cpp
@@ -0,0 +1,26 @@
+
+
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+//     int prod1=1,prod2=1;
+//     vector <int> a= {1,2,3,4,5};
+//     int n=a.size();
+//     vector <int> left(n),right(n);
+    
+//     for (int i=0;i<n;i++)
+//     {
+//         prod1=prod1*a[i];
+//         left[i]=prod1;
+//         prod2=prod2*a[n-i-1];
+//         right[n-i-1]=prod2;
+//     }
+// for (auto i:right)
+// cout<<i<<" ";
+    int a=3,b=7;
+    cout<<(~a);
+    return 0;
+}
