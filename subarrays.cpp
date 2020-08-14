bf99c35b875f38730c248969d2a8d5cae4b0841e
Gunisha Chaturvedi <gunisha3007@gmail.com>
2020-08-14 11:02:20 +0530

    added recursive method

--- subarrays.cpp
+++ subarrays.cpp
@@ -1,3 +1,4 @@
+//iterative
 #include <iostream>
 
 using namespace std;
@@ -15,6 +16,37 @@ int main()
         }
     }
     
+
+    return 0;
+}
+
+//recursive
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(vector <int> ar, int st, int en)
+{
+    if(en==ar.size())
+    return;
+    
+    else if(st>en)
+    solve(ar,0,en+1);
+    
+    else
+    {
+        cout<<"[ ";
+        for (int i=st;i<en;i++)
+        cout<<ar[i]<<" ";
+    
+        cout<<ar[en]<<" ]"<<endl;
+        solve(ar,st+1,en);
+    }
+}
+
+int main()
+{
+    vector <int> ar={1,2,3,4};
+    solve(ar,0,0);
 
     return 0;
 }
