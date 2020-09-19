a4fa19bc3082f9caf55f29cc116709d97477a463
Gunisha Chaturvedi <gunisha3007@gmail.com>
2020-09-19 23:36:34 +0530

    reverse bits of an integer

---
+++ reversebits.cpp
@@ -0,0 +1,21 @@
+//reverse digits
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+int i=0;
+unsigned long int a, num=0;
+cin>>a;
+while(a)
+{
+    //shifting is faster than pow function 
+    num=num+((a%2)*1<<(3-i));
+    a=a/2;
+    i++;
+}
+cout<<num;
+//cout<<((1<<3)+10); 
+return 0;
+}
